e1656a9e9ad056b7f5a674f6a27dc536bd9875a9
Eugene <eugene@dragonwebdesign.co.za>
2023-11-27 12:11:19 +0200

    first commitShopify

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './sippets/*.liquid',
+    './templates/customers/*.liquid',
+    './templates/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
